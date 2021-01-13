258f7df76e04bce5056c320504cf790bcc6dfd81
Austin Landis <austinlandiscode@gmail.com>
2021-01-13 11:07:12 -0800

    Add .toUpperCase
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const name1Input = $("input#name1").val();
-    const name2Input = $("input#name2").val();
-    const adjectiveInput = $("input#adjective").val();
-    const animalsInput = $("input#animals").val();
+    const name1Input = $("input#name1").val().toUpperCase();
+    const name2Input = $("input#name2").val().toUpperCase();
+    const adjectiveInput = $("input#adjective").val().toUpperCase();
+    const animalsInput = $("input#animals").val().toUpperCase();
 
     $(".name1").text(name1Input);
     $(".name2").text(name2Input);
